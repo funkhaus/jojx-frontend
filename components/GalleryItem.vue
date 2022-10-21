08d9bd660dd0145cd54c58d1244e4cf9af07eded
dchiamp <derek@funkhaus.us>
2022-10-21 12:46:57 -0700

    update intro gallery item

--- components/GalleryItem.vue
+++ components/GalleryItem.vue
@@ -10,7 +10,7 @@
             <wp-image
                 class="image"
                 :image="image"
-                object-fit="contain"
+                object-fit="cover"
                 background-color="transparent"
             />
         </nuxt-link>
